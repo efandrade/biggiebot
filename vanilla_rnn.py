8e8ecc99b47ec2664f272fb8f83db8d601f3a9e2
efandrade <Erick.F.Andrade+github@gmail.com>
2018-09-28 18:44:39 -0400

    comments

--- vanilla_rnn.py
+++ vanilla_rnn.py
@@ -5,32 +5,44 @@
 
 import numpy as np
 
+#Generates a sequence using the output of the train algorithm
 def prediction(h, seed, dim, length, Whh, Wxh, bh, Why, by):
+    #h:         Previous hidden state
+    #seed:      Initial input to begin squence
+    #dim:       Number of features
+    #length:    Lenght of sequence to be generated
+    
+    x = np.zeros([dim,1])
+    x[seed,0] = 1
+    predic = []
+    WhhWxh = np.concatenate((Whh,Wxh),axis=1)
+    for i in range(length):
+        hx = np.concatenate((h,x),axis=0)
+        h = np.tanh(np.dot(WhhWxh,hx) + bh)
+        y = np.dot(Why,h) + by
+        p = np.exp(y)/np.sum(np.exp(y))
+        ix = np.random.choice(range(dim),p=p.ravel())
         x = np.zeros([dim,1])
-        x[seed,0] = 1
-        predic = []
-        WhhWxh = np.concatenate((Whh,Wxh),axis=1)
-        for i in range(length):
-            hx = np.concatenate((h,x),axis=0)
-            h = np.tanh(np.dot(WhhWxh,hx) + bh)
-            y = np.dot(Why,h) + by
-            p = np.exp(y)/np.sum(np.exp(y))
-            ix = np.random.choice(range(dim),p=p.ravel())
-            x = np.zeros([dim,1])
-            x[ix] = 1
-            predic.append(ix)
-        return predic
+        x[ix] = 1
+        predic.append(ix)
+    return predic
     
 def pred2str(predic,ind_dict):
     txt = ''.join(ind_dict[ix] for ix in predic)
     return txt
 
+#Calculates the 
 def lossFun(inputs, targets, init_h, hidden_layer_size, charVec, Whh, Wxh, bh, Why, by):
 
-#forward propagation
+    ###forward propagation###
+    
+    #number of inputs
     input_length = len(inputs)
-    xs = charVec[inputs].T
+    xs = charVec[inputs].T                              
+    #create hidden layer x number of inputs matrix
     hs = np.zeros([hidden_layer_size,input_length])
+    #add initial hidden layer to the end of hs matrix for coveience in the for
+    #loop below
     hs[:,-1] = init_h[:,0]
     loss = 0
     
