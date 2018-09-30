9d349ebe49da2f319a63f94c6cc8be0efff736bb
efandrade <Erick.F.Andrade+github@gmail.com>
2018-09-30 19:39:43 -0400

    added dictionary by words

--- create_dictionary.py
+++ create_dictionary.py
@@ -2,16 +2,31 @@
 
 import numpy as np
 
-#Opens and reads text, returns corresponding dictionaries for text file
+#Opens and reads text, returns character dictionaries for text file
 def bychar(filename):
     #filename:  Name of the text file to open
     
-    lyrics = open(filename,'r').read()                                          #read text file
-    chars = list(set(lyrics))                                                   #lists used charachters in text file
-    char_dict = {char:ind for ind,char in enumerate(chars)}                     #create a dictionary for converting character to corresponding number (index)
-    ind_dict = {ind:char for ind,char in enumerate(chars)}                      #create a dictionary for converting number (index) to corresponding character
-    num_uniq_chars = len(chars)                                                 #number of unique characters in text file
-    num_chars = len(lyrics)                                                     #number of total characters in text file
-    charmat = np.identity(num_uniq_chars)                                       #Matrix where the columns correspont to the vectors representation of the each entry (characters) in the dictionary
+    lyrics = open(filename,'r').read()                          #read text file
+    chars = list(set(lyrics))                                   #lists charachters in text file
+    char_dict = {char:ind for ind,char in enumerate(chars)}     #create a dictionary for converting characters to corresponding numbers (index)
+    ind_dict = {ind:char for ind,char in enumerate(chars)}      #create a dictionary for converting numbers (index) to corresponding characters
+    num_uniq_chars = len(chars)                                 #number of unique characters in text file
+    num_chars = len(lyrics)                                     #number of total characters in text file
+    charmat = np.identity(num_uniq_chars)                       #Matrix where the columns correspont to the vectors representation of the each entry (characters) in the dictionary
     
     return charmat, char_dict, ind_dict, num_uniq_chars, num_chars
+
+#Opens and reads text, returns word dictionaries for text file
+def byword(filename):
+    #filename:  Name of the text file to open
+    
+    lyrics = open(filename,'r').read()                          #read text file
+    lyrics = lyrics.split()                                     #creates array of words
+    words = list(set(lyrics))                                   #lists words in text file
+    word_dict = {word:ind for ind,word in enumerate(words)}     #create a dictionary for converting words to corresponding number (index)
+    ind_dict = {ind:word for ind,word in enumerate(words)}      #create a dictionary for converting numbers (index) to corresponding characters
+    num_uniq_words = len(words)                                 #number of unique words in text file
+    num_words = len(lyrics)                        #number of total words in text file
+    wordmat = np.identity(num_uniq_words)                       #Matrix where the columns correspont to the vectors representation of the each entry (words) in the dictionary
+    
+    return lyrics, wordmat, word_dict, ind_dict, num_uniq_words, num_words
--- vanilla_rnn.py
+++ vanilla_rnn.py
@@ -2,7 +2,6 @@
 
 import numpy as np
 
-charVec = np.identity(num_uniq_chars)
 #Generates a sequence using the output of the train algorithm
 def prediction(s_tm1, seed, dim, length, W, U, sbias, V, ybias,random=True):
     #s_tm1:     Previous state
@@ -36,11 +35,15 @@ def prediction(s_tm1, seed, dim, length, W, U, sbias, V, ybias,random=True):
     return predict
 
 #Coverts sequence of numbers to the corresponding characters
-def pred2str(predict,ind_dict):
+def pred2str(predict,ind_dict,space=False):
     #predict:       Sequence of numbers
     #ind_dict:      Corresponding numbers to characters
     
-    txt = ''.join(ind_dict[ix] for ix in predict)                #coverts sequence of numbers to the corresponding characters
+    if space == False:
+        txt = ''.join(ind_dict[ix] for ix in predict)                #coverts sequence of numbers to the corresponding characters
+    else:
+        txt = ''.join([ind_dict[ix] + ' '][0] for ix in predict)     #coverts sequence of numbers to the corresponding characters with spaces inbetween
+        
     return txt
 
 def CostFun(inputs, targets, s_m1, hidden_layer_size, charVec, W, U, sbias, V, ybias):
@@ -100,56 +103,3 @@ def CostFun(inputs, targets, s_m1, hidden_layer_size, charVec, W, U, sbias, V, y
 
     return loss, dU, dW, dV, dsbias, dybias, next_s_m1
 
-
-
-def lossFun_old(inputs, targets, init_h, hidden_layer_size, charVec, Whh, Wxh, bh, Why, by):
-
-    ###forward propagation###
-    
-    #number of inputs
-    input_length = len(inputs)
-    xs = charVec[inputs].T                              
-    #create hidden layer x number of inputs matrix
-    hs = np.zeros([hidden_layer_size,input_length])
-    #add initial hidden layer to the end of hs matrix for coveience in the for
-    #loop below
-    hs[:,-1] = init_h[:,0]
-    loss = 0
-    
-    WhhWxhbh = np.concatenate((Whh,Wxh,bh),axis=1)
-    for i in range(0,len(inputs)):
-        hx1 = np.concatenate((hs[:,i-1],xs[:,i],np.ones(1)),axis=0)
-        hs[:,i] = np.tanh(np.dot(WhhWxhbh,hx1))
-        
-    ys = np.dot(Why,hs) + np.tile(by,input_length)
-    ps = np.exp(ys) / np.sum(np.exp(ys),axis=0)
-    loss += np.sum(-np.log(ps[targets,range(input_length)]))
-    
-    y = np.expand_dims(hs[:,-1],axis=1)
-    
-#back propagation
-    dWxh = np.zeros(Wxh.shape)
-    dWhh = np.zeros(Whh.shape)
-    dWhy = np.zeros(Why.shape)
-    
-    dbh = np.zeros(bh.shape)
-    dby = np.zeros(by.shape)
-    dhnext = np.expand_dims(np.zeros(hs[:,0].shape),axis=1)
-    
-    dy = np.copy(ps) - charVec[targets].T
-    dWhy += np.dot(dy,hs.T)
-    dby = np.sum(dy,axis=1)[:,np.newaxis]
-    hs[:,-1] = init_h[:,0]
-    
-    for i in range(len(inputs))[::-1]:
-        dh = np.dot(Why.T, dy[:,i][:,np.newaxis]) + dhnext
-        dhraw = (1 - np.expand_dims(hs[:,i] * hs[:,i],axis=1)) * dh
-        dbh += dhraw
-        dWxh += np.dot(dhraw, xs[:,i][:,np.newaxis].T)
-        dWhh += np.dot(dhraw, hs[:,i-1][:,np.newaxis].T)
-        dhnext = np.dot(Whh.T, dhraw)
-
-    for dparam in [dWxh,dWhh,dWhy,dbh,dby]:
-        np.clip(dparam,-5,5,out=dparam)
-
-    return loss, dWxh, dWhh, dWhy, dbh, dby, y
