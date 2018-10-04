6b8496fb3536d6bfa2aff08a7a8017446c74bf51
efandrade <Erick.F.Andrade+github@gmail.com>
2018-10-04 19:50:45 -0400

    updated

--- scraper.py
+++ scraper.py
@@ -26,10 +26,11 @@ def artistlyrics(filename,song=0):
     database = json.loads(jsonfile)
     artist = database['artist']
     lyrics = database['songs'][song]['lyrics']
+    featartist = database['songs'][song]['raw']['featured_artists']
     
-    length = len(lyrics)
     sections = []
     x1, x2 = 0, 0
+    pos = 0
     artistlyrics = []
     
     while x1 != -1:
@@ -37,18 +38,19 @@ def artistlyrics(filename,song=0):
         x2 = lyrics.find(']',pos)
         if x1 != -1:
             sections.append([lyrics[x1:x2+1],x1,x2])
+        pos = x2 + 2
         
     for i in range(len(sections)):
         if sections[i][0].lower().find('verse') != -1:
             start = sections[i][2]+2
             if i == len(sections):
                 end = len(lyrics)
-                else:
-                    end = sections[i+1][1]           
+            else:
+                end = sections[i+1][1]
             if featartist != []:
                 if sections[i][0].find(artist) != -1:
                     artistlyrics.append(lyrics[start:end])
-                    else:
-                        artistlyrics.append(lyrics[start:end])
+            else:
+                artistlyrics.append(lyrics[start:end])
 
     return ''.join(artistlyrics)
